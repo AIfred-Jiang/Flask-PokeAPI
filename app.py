7a30402624db6693f2b3fe26db64df3929a001c9
AIfred-Jiang <alfredj14@nycstudents.net>
2025-05-21 10:12:39 -0400

    commit

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+from urllib.parse import quote
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    
-    characters = [] 
+@app.template_filter('urlencode')
+def urlencode_filter(s):
+    return quote(s)
 
+
+def get_all_characters():
+    characters = []
     for page in range(1, 11):
         response = requests.get(f"https://rickandmortyapi.com/api/character?page={page}")
         data = response.json()
-        characters.extend(data['results']) 
+        characters.extend(data['results'])
+    return characters
 
-
+@app.route('/')
+def index():
+    characters = get_all_characters()
     return render_template('index.html', characters=characters)
 
+@app.route('/search')
+def search():
+    query = request.args.get('query', '').lower()
+    characters = get_all_characters()
+
+    filtered = [
+        char for char in characters
+        if query in char['name'].lower()
+        or query in char['species'].lower()
+        or query in char['status'].lower()
+    ]
+
+    if not filtered:
+        return render_template('error.html', message=f"No results found for '{query}'."), 404
+
+    return render_template('search.html', characters=filtered, query=query)
 
 @app.route('/character/<int:id>')
 def character_detail(id):
